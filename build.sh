5d1a269fd8e1894ad1901647096a3d71fea46587
composer <2789706336@qq.com>
2023-02-22 10:12:15 +0800

    fix: build and run script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 cd $(dirname $0)
 
 cd ./gateway
-go build -o ../gateway
+go build -o ../gateway_build
 cd ../
 
 cd ./message
-go build -o ../message
+go build -o ../message_build
 cd ../
 
 cd ./user
-go build -o ../user
+go build -o ../user_build
 cd ../
 
 cd ./video
-go build -o ../video
+go build -o ../video_build
 cd ../
--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 cd $(dirname $0)
 
-nohup sh ./gateway > ./gateway.log 2>&1 &
+nohup sh ./gateway_build > ./gateway.log 2>&1 &
 
-nohup sh ./message > ./message.log 2>&1 &
+nohup sh ./message_build > ./message.log 2>&1 &
 
-nohup sh ./user > ./user.log 2>&1 &
+nohup sh ./user_build > ./user.log 2>&1 &
 
-nohup sh ./video > ./video.log 2>&1 &
+nohup sh ./video_build > ./video.log 2>&1 &
